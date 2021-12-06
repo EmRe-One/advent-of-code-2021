0b9ee32319424a8b499fcf10f5e11e685f5c575a
Emre Akgül <emre.akguel.1@gmail.com>
2021-12-06 04:12:42 +0100

    implement gradleTask for preparing next day

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,3 +29,32 @@ tasks {
         gradleVersion = "7.3"
     }
 }
+
+tasks.register("prepareNextDay") {
+    doLast {
+        val nextDay = 6
+        val withTest = true
+        val packageId = "de.emreak.adventofcode"
+
+        val newSrcFile = """$projectDir/src/main/kotlin/${packageId.replace(".", "/")}/days/Day${nextDay}.kt"""
+        val newTestFile = """$projectDir/src/test/kotlin/${packageId.replace(".", "/")}/days/Day${nextDay}Test.kt"""
+
+        if (file(newSrcFile).exists()) {
+            println("$newSrcFile already exists")
+        }
+        else {
+            file(newSrcFile).writeText(
+                file("""$projectDir/template/DayX.kt""").readText().replace("$1", "$nextDay")
+            )
+            file("""$projectDir/src/main/resources/day$nextDay.txt""").writeText("")
+
+            if (withTest) {
+                file(newTestFile).writeText(
+                    file("""$projectDir/template/DayXTest.kt""").readText().replace("$1", "$nextDay")
+                )
+
+                file("""$projectDir/src/test/resources/day${nextDay}_example.txt""").writeText("")
+            }
+        }
+    }
+}
