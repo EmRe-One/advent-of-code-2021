7e8d191f14fedb43787cf16474a184ce3b8b0151
Emre Akgül <emre.akguel.1@gmail.com>
2021-12-07 14:44:34 +0100

    Solve day 7

--- src/main/kotlin/de/emreak/adventofcode/days/Day7.kt
+++ src/main/kotlin/de/emreak/adventofcode/days/Day7.kt
@@ -1,14 +1,40 @@
 package de.emreak.adventofcode.days
 
+import kotlin.math.abs
+
 object Day7 {
 
     fun part1(input: List<String>): Int {
+        val positions = input[0].split(",").map { it.toInt() }
+        val min = positions.minOf { it }
+        val max = positions.maxOf { it }
 
-        return 0
+        var minFuel = Int.MAX_VALUE
+        for(i in min..max) {
+            val totalFuel = positions.sumOf { abs(it - i) }
+            if(totalFuel < minFuel) {
+                minFuel = totalFuel
+            }
+        }
+        return minFuel
     }
 
     fun part2(input: List<String>): Int {
+        val positions = input[0].split(",").map { it.toInt() }
+        val min = positions.minOf { it }
+        val max = positions.maxOf { it }
 
-        return 0
+        var minFuel = Int.MAX_VALUE
+        for(i in min..max) {
+            // fuel costs -> 1 + 2 + 3 + 4 + 5  -> sum (i = 1..n) -> i -> n * (n + 1) / 2
+            val totalFuel = positions.sumOf { it ->
+                val n = abs(it - i)
+                n * (n + 1) / 2
+            }
+            if(totalFuel < minFuel) {
+                minFuel = totalFuel
+            }
+        }
+        return minFuel
     }
 }
--- src/test/kotlin/de/emreak/adventofcode/days/Day7Test.kt
+++ src/test/kotlin/de/emreak/adventofcode/days/Day7Test.kt
@@ -8,13 +8,13 @@ internal class Day7Test {
     @Test
     fun part1() {
         val input = AdventOfCodeUtils.readLines("src/test/resources", "day7_example.txt")
-        assertEquals(-1, Day7.part1(input), "Day7, Part1 should be -1.")
+        assertEquals(37, Day7.part1(input), "Day7, Part1 should be 37.")
     }
 
     @Test
     fun part2() {
         val input = AdventOfCodeUtils.readLines("src/test/resources", "day7_example.txt")
-        assertEquals(-1, Day7.part2(input), "Day7, Part2 should be -1.")
+        assertEquals(168, Day7.part2(input), "Day7, Part2 should be -1.")
     }
 
 }
