17f129fc5832b9a0c86340e6189ae38b24d0abe8
Emre Akgül <emre.akguel.1@gmail.com>
2021-12-12 08:14:52 +0100

    Finish day 11

--- src/test/kotlin/de/emreak/adventofcode/days/Day11Test.kt
+++ src/test/kotlin/de/emreak/adventofcode/days/Day11Test.kt
@@ -8,13 +8,13 @@ internal class Day11Test {
     @Test
     fun part1() {
         val input = AdventOfCodeUtils.readLines("src/test/resources", "day11_example.txt")
-        assertEquals(-1, Day11.part1(input), "Day11, Part1 should be -1.")
+        assertEquals(1656, Day11.part1(input), "Day11, Part1 should be 1656.")
     }
 
     @Test
     fun part2() {
         val input = AdventOfCodeUtils.readLines("src/test/resources", "day11_example.txt")
-        assertEquals(-1, Day11.part2(input), "Day11, Part2 should be -1.")
+        assertEquals(195, Day11.part2(input), "Day11, Part2 should be 195.")
     }
 
 }
