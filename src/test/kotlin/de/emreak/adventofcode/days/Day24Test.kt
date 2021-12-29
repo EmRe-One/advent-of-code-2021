46856e4ce494f733e1681c1417c02aa1f8b399fd
Emre Akgül <emre.akguel.1@gmail.com>
2021-12-29 18:25:37 +0100

    Finish day 24

--- src/test/kotlin/de/emreak/adventofcode/days/Day24Test.kt
+++ src/test/kotlin/de/emreak/adventofcode/days/Day24Test.kt
@@ -1,6 +1,5 @@
 package de.emreak.adventofcode.days
 
-import de.emreak.adventofcode.AdventOfCodeUtils
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -8,14 +7,7 @@ internal class Day24Test {
 
     @Test
     fun part1() {
-        val input = AdventOfCodeUtils.readLines("src/test/resources", "day24_example.txt")
-        assertEquals(-1, Day24.part1(input), "Day24, Part1 should be -1.")
-    }
-
-    @Test
-    fun part2() {
-        val input = AdventOfCodeUtils.readLines("src/test/resources", "day24_example.txt")
-        assertEquals(-1, Day24.part2(input), "Day24, Part2 should be -1.")
+        assertTrue(true, "Day24, no test available.")
     }
 
 }
